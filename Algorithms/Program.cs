66a748d36341a902c8faa3271db1ed328e178318
pixcelo <bodomu@gmail.com>
2022-10-10 13:44:24 +0900

    add

---
+++ Algorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Algorithms.Service;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var ss = new SortService();
+            ss.InsertionSort();
+        }
+
+    
+    }
+}
---
+++ Algorithms/Service/SortService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Algorithms.Service
+{
+    public class SortService
+    {
+        // Insertion sort
+        public void InsertionSort()
+        {
+            var list = new List<int>() { 7, 2, 6, 9, 5 };
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                int tmp = list[i];
+                int j;
+
+                for (j = i - 1; j >= 0; j--)
+                {
+                    if (list[j] > tmp)
+                    {
+                        list[j + 1] = list[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                list[j + 1] = tmp;
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
